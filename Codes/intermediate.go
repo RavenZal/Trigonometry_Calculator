e8b50d0af346cdbccb0c02cbda7c574342012869
RavenZa1 <osadaka@outlook.com>
2025-06-03 11:12:17 +0800

    Add files via upload
    primary structure

---
+++ Codes/intermediate.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+)
+
+type CalculationRequest struct {
+	Angle    float64 `json:"angle"`
+	Unit     string  `json:"unit"`
+	Function string  `json:"function"`
+}
+
+type CalculationResponse struct {
+	Result float64 `json:"result,omitempty"`
+	Error  string  `json:"error,omitempty"`
+}
+
+func main() {
+	http.HandleFunc("/calculate", handleCalculate)
+	log.Println("Go中间层启动，监听 :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleCalculate(w http.ResponseWriter, r *http.Request) {
+	//todo:请求格式有误
+	//todo:参数有误
+	//todo:服务器连接失败
+	//todo:请求发送失败
+	//todo：结果读取失败
+	//todo：无效的计算结果
+}
+
+func sendSuccess(w http.ResponseWriter, result float64) {
+//todo: return success 
+}
+
+func sendError(w http.ResponseWriter, msg string, statusCode int) {
+//todo: return error and error type?
+}
