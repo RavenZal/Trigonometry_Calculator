bae9a73926fd067d621627eece5b1df08ee2dfb7
tong-l-c <970049957@qq.com>
2025-06-17 17:52:14 +0800

    Update calculator.m

--- Doc/calculator.m
+++ Doc/calculator.m
@@ -1,15 +1,31 @@
     clear; close all; clc;
     %%TODO:接口
-    % 用户输入类型
-    type_input = input('请输入角度或数值，若角度输入1,数值输入2。', 's');%%输入角度计算三角函数，输入数值计算反三角函数
-    switch type_input
-        case '1'
-        % TODO:输入角度情况下，检测输入，并计算三角函数
+    % 数据结构要求 字段顺序：函数名,角度值,单位数值;精度：8位小数;单位限制：仅接受degree或radian
+    %%TODO:MATLAB自带函数的边界值处理
+    if  type_input(3)==degree
+       type_input(2)=2*pi*type_input(2)/360;%将角度转化为弧度
+    end
+    switch type_input(1)
+        case 'sin'
+            ans=sin(type_input(2));
+        case 'cos'
+            ans=cos(type_input(2));
+        case 'tan'
+            if(type_input(2))==(pi/2)||(3*pi/2)%%TODO:计算tan函数时，输入非法值时处理
+
+            end
+            ans=tan(type_input(2));
+        case 'cot'
+            if(type_input(2))==(pi/2)||(3*pi/2)%%TODO:计算cot函数时，输入非法值时处理
+            
+            end 
+            ans=cot(type_input(2));
+        % 输入角度情况下，检测输入，并计算三角函数
             
             
-        case '2'
+        case 
             %TODO:输入数值情况下，检测输入，并计算反三角函数
         otherwise
-            fprintf('输入无效，请输入角度或数值，若角度输入1,数值输入2。\n');
+           %TODO:输入错误情况下的处理
     end 
     %%TODO:接口
